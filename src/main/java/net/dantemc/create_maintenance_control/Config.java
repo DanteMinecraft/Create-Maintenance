0456655a4e398d849526cd3b661beeda68a0bccb
DanteMinecraft <dantewallen07@gmail.com>
2026-06-13 15:37:55 +0200

    updated config class to forge

--- src/main/java/net/dantemc/create_maintenance_control/Config.java
+++ src/main/java/net/dantemc/create_maintenance_control/Config.java
@@ -2,16 +2,16 @@ package net.dantemc.create_maintenance_control;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.common.ModConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public class Config {
-    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ModConfigSpec.BooleanValue WRITE_DEBUG_LOGS = BUILDER
+    public static final ForgeConfigSpec.BooleanValue WRITE_DEBUG_LOGS = BUILDER
             .comment("Enable debug logging for Create: Maintenance Control")
             .define("debugLogs", false);
 
-    static final ModConfigSpec SPEC = BUILDER.build();
+    static final ForgeConfigSpec SPEC = BUILDER.build();
 
     private static boolean validateItemName(final Object obj) {
         return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
