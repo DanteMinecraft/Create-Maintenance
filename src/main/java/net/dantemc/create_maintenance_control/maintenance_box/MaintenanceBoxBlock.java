89616859c327763cbc289c7cf09ed14e50ceda23
DanteMinecraft <dantewallen07@gmail.com>
2026-06-14 17:51:46 +0200

    added powered state to maintenance box

--- src/main/java/net/dantemc/create_maintenance_control/maintenance_box/MaintenanceBoxBlock.java
+++ src/main/java/net/dantemc/create_maintenance_control/maintenance_box/MaintenanceBoxBlock.java
@@ -11,16 +11,29 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class MaintenanceBoxBlock extends WrenchableDirectionalBlock implements IBE<MaintenanceBoxBlockEntity> {
 
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
     public MaintenanceBoxBlock(Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(POWERED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+        super.createBlockStateDefinition(builder);
     }
 
     @Override
